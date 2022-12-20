67d6bda9acd4a13b61975c5f740ab667308bc9db
Manjukh <manjukasthuri@yahoo.com>
2022-12-20 23:12:31 +0000

    CharDataType1

---
+++ CharDataType1.java
@@ -0,0 +1,9 @@
+public class CharDataType1{
+    public static void main(String args[]){
+        char marks,grade;
+        marks='2';
+        grade='B';
+        System.out.println("Marks:" +marks);
+        System.out.println("Grade:"  +grade);
+    }
+}
